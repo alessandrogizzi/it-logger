04636709f07e74255c6784846a10e0d255710853
carlopisacane <doncarlopisacane@gmail.com>
2019-10-13 00:36:40 +0200

    Add Logs

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -11,8 +13,17 @@ const AddLogModal = () => {
     if(message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(message, tech, attention);
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
       
+      addLog(newLog);
+
+      M.toast({ html: `Log added by ${tech}`});
+
       //Clear Fields
       setMessage('');
       setTech('');
@@ -51,7 +62,7 @@ const AddLogModal = () => {
                   John Doe
                 </option>
                 <option value="Sam Smith">
-                  John Doe
+                  Sam Smith
                 </option>
                 <option value="Sara Wilson">
                   Sara Wilson
@@ -89,9 +100,13 @@ const AddLogModal = () => {
   );
 };
 
+AddLogModal.protoTypes = {
+  addLog: PropTypes.func.isRequired,
+}
+
 const modalStyle = {
   width: '75%',
   height: '75%'
 }
 
-export default AddLogModal
+export default connect(null, { addLog })(AddLogModal);
