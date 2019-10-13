d69309ff60b4ad6c56c8b06a184d51f4739305b6
carlopisacane <doncarlopisacane@gmail.com>
2019-10-13 23:24:23 +0200

    Add Technicians & Delete Technicians

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -3,7 +3,7 @@ import {
   ADD_TECH,
   DELETE_TECH,
   SET_LOADING,
-  TECHS_ERROR
+  TECHS_ERROR,
 } from './types';
 
 // Get techs from server
@@ -17,6 +17,54 @@ export const getTechs = () => async dispatch => {
     dispatch({
       type: GET_TECHS,
       payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
+
+// Add new Tech
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/techs', {
+      method: 'POST',
+      body: JSON.stringify(tech),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
+
+// Delete Tech
+export const deleteTech = id => async dispatch => {
+
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
     });
   } catch (err) {
     dispatch({
--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -11,7 +11,7 @@ const AddLogModal = ({ addLog }) => {
   const [tech, setTech] = useState('');
 
   const onSubmit = () => {
-    if(message === '' || tech === '') {
+    if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
       const newLog = {
@@ -20,56 +20,59 @@ const AddLogModal = ({ addLog }) => {
         tech,
         date: new Date()
       };
-      
+
       addLog(newLog);
 
-      M.toast({ html: `Log added by ${tech}`});
+      M.toast({ html: `Log added by ${tech}` });
 
-      //Clear Fields
+      // Clear Fields
       setMessage('');
       setTech('');
       setAttention(false);
     }
-  }
+  };
 
   return (
-    <div id='add-log-modal' className="modal" style={modalStyle}>
-      <div className="modal-content">
+    <div id='add-log-modal' className='modal' style={modalStyle}>
+      <div className='modal-content'>
         <h4>Enter System Log</h4>
-        <div className="row">
-          <div className="input-field">
-            <input 
-              type="text" 
-              name="message" 
-              value={message} 
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='message'
+              value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor="message" className="active">
+            <label htmlFor='message' className='active'>
               Log Message
             </label>
           </div>
         </div>
-        <div className="row">
-          <div className="input-field">
-            <select 
-              name="tech" 
-              value={tech} 
-              className='bowser-default' 
-              onChange={e => setTech(e.target.value)}>
-                <option value="" disabled>
-                  Select Technician
-                </option>
-                <TechSelectOptions />
+
+        <div className='row'>
+          <div className='input-field'>
+            <select
+              name='tech'
+              value={tech}
+              className='browser-default'
+              onChange={e => setTech(e.target.value)}
+            >
+              <option value='' disabled>
+                Select Technician
+              </option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
-        <div className="row">
-          <div className="input-field">
+
+        <div className='row'>
+          <div className='input-field'>
             <p>
               <label>
-                <input 
-                  type="checkbox" 
-                  className="filled-in" 
+                <input
+                  type='checkbox'
+                  className='filled-in'
                   checked={attention}
                   value={attention}
                   onChange={e => setAttention(!attention)}
@@ -80,11 +83,11 @@ const AddLogModal = ({ addLog }) => {
           </div>
         </div>
       </div>
-      <div className="modal-footer">
-        <a 
-          href="#!" 
-          onClick={onSubmit} 
-          className="modal-close waves-effect blue waves-light btn"
+      <div className='modal-footer'>
+        <a
+          href='#!'
+          onClick={onSubmit}
+          className='modal-close waves-effect blue waves-light btn'
         >
           Enter
         </a>
@@ -93,13 +96,16 @@ const AddLogModal = ({ addLog }) => {
   );
 };
 
-AddLogModal.protoTypes = {
-  addLog: PropTypes.func.isRequired,
-}
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired
+};
 
 const modalStyle = {
   width: '75%',
   height: '75%'
-}
+};
 
-export default connect(null, { addLog })(AddLogModal);
+export default connect(
+  null,
+  { addLog }
+)(AddLogModal);
